ee179133f21f3514c3b398486cb1459bbae179e6
Alessandro Jean <alessandrojean@gmail.com>
2021-05-21 19:04:13 -0300

    Add some initial commands (#1)
    Add support to lock and duplicate commands.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# issue-moderator-action
+# GitHub action to moderate issues
+
+This action allows the usage of some commands from the organization
+to moderate the issues of a repository.
+
+## Installation
+
+To configure the action simply add the following lines to your workflow file:
+
+```yml
+name: Moderator
+on:
+  issue_comment:
+    types: [created]
+jobs:
+  moderate:
+    runs-on: ubuntu-latest
+    steps:
+    - name: Moderate issues
+      uses: tachiyomiorg/issue-moderator-action
+      with:
+        repo-token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+## Inputs
+
+| Name | Description | Default value |
+| ---- | ----------- | ------------- |
+| `repo-token` | GitHub token |  |
+| `lock-command` | Optional lock command text. | Lock this issue |
+| `duplicate-command` | Optional duplicate command text. | Duplicate of # |
---
+++ src/main.ts
@@ -0,0 +1,100 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import { GitHub } from '@actions/github/lib/utils';
+
+type GitHubClient = InstanceType<typeof GitHub>
+type LockReason = 'off-topic' | 'too heated' | 'resolved' | 'spam'
+
+const COMMANDS: Record<string, (client: GitHubClient) => Promise<void>> = {
+  'lock': lockIssue,
+  'duplicate': duplicateIssue
+}
+
+const ALLOWED_ACTIONS = ['created'];
+
+async function run() {
+  try {
+    const { actor, payload, repo } = github.context;
+
+    // Do nothing if it's wasn't a relevant action or it's not an issue comment.
+    if (ALLOWED_ACTIONS.indexOf(payload.action) !== -1 || !payload.comment || !payload.issue) {
+      return;
+    }
+    if (!payload.sender) {
+      throw new Error('Internal error, no sender provided by GitHub');
+    }
+
+    const commentBody: string = payload.comment.body;
+
+    // Find the command used.
+    const commandToRun = Object.keys(COMMANDS)
+      .find(key => {
+        return commentBody.startsWith(core.getInput(`${key}-command`));
+      });
+
+    if (commandToRun) {
+      core.info(`Command found: ${commandToRun}`);
+
+      const client = github.getOctokit(
+        core.getInput('repo-token', {required: true})
+      );
+
+      // Get all the members from the organization.
+      const allowedMembers = await client.rest.orgs.listMembers({
+        org: repo.owner
+      });
+
+      if (allowedMembers.status !== 200) {
+        core.info('Failed to fetch the members from the organization');
+        return;
+      }
+
+      if (allowedMembers.data.find(member => member.login === actor)) {
+        await COMMANDS[commandToRun](client);
+      }  
+    } 
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function lockIssue(client: GitHubClient) {
+  const { payload, repo } = github.context;
+  const commentBody: string = payload.comment.body;
+
+  const lockReasons = ['off-topic', 'too heated', 'resolved', 'spam'];
+
+  const reason = lockReasons.find(option => commentBody.includes(option));
+
+  await client.rest.issues.lock({
+    owner: repo.owner,
+    repo: repo.repo,
+    issue_number: payload.issue.number,
+    lock_reason: reason ? reason as LockReason : undefined
+  });
+
+  core.info(`Issue #${payload.issue.number} locked`);
+}
+
+async function duplicateIssue(client: GitHubClient) {
+  const { issue, payload, repo } = github.context;
+
+  const issueMetadata = {
+    owner: issue.owner,
+    repo: issue.repo,
+    issue_number: issue.number
+  };
+
+  const issueData = await client.rest.issues.get(issueMetadata);
+
+  if (issueData.data.state === 'open') {
+    await client.rest.issues.update({
+      owner: payload.issue.owner,
+      repo: payload.issue.repo,
+      issue_number: payload.issue.number,
+      state: 'closed'
+    });
+
+    core.info(`Issue #${payload.issue.number} closed`);
+  }
+}
