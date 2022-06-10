eef81c8e2a94f1c508fe9eccf4beed938e5b1cf8
Alessandro Jean <14254807+alessandrojean@users.noreply.github.com>
2022-06-10 12:50:41 -0300

    Remove double duplicate comments as it's not needed anymore.

--- src/feature/dupe-check.ts
+++ src/feature/dupe-check.ts
@@ -91,22 +91,12 @@ export async function checkForDuplicates() {
     .join(', ')
     .replace(/, ([^,]*)$/, ' and $1');
 
-  // The 'Duplicate of' keyword only works if its only it in the comment.
-  // We create this separate comment to proper create the issue timeline event.
-  // Reference: https://github.com/githubteacher/github-for-developers-sept-2015/issues/705#issuecomment-380805734
-  await client.rest.issues.createComment({
-    ...issueMetadata,
-    // For now just use the more recent duplicate issue.
-    body: `Duplicate of ${duplicateIssues[0]}`,
-  });
-
   await client.rest.issues.update({
     ...issueMetadata,
     state: 'closed',
     state_reason: 'not_planned',
   });
 
-  // Also create a more explained comment to the end user.
   await client.rest.issues.createComment({
     ...issueMetadata,
     body: dedent`
