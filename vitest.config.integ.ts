87cd28af9371373b13b992d83b8515359122f025
arkon <eugcheung94@gmail.com>
2023-04-02 09:27:42 -0400

    Remove need for unit test config

--- vitest.config.integ.ts
+++ vitest.config.integ.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['integ/**/*.test.ts'],
+    include: ['integ/**/*.integ.ts'],
     testTimeout: 20_000,
   },
 });
